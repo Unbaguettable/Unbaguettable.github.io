d6c593abdfb2ec3dfe638322f20e7571c89e6ce4
Unbaguettable <44548571+Unbaguettable@users.noreply.github.com>
2022-02-22 16:22:40 +0100

    test 3
    electric uhh what rhymes with 3

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ var cacheName = 'v1:static';
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
     // once the SW is installed, go ahead and fetch the resources to make this work offline
-    e.waitUntil(
+    /*e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll([
                 './',
@@ -17,7 +17,7 @@ self.addEventListener('install', function(e) {
                 self.skipWaiting();
             });
         })
-    );
+    );*/
 });
 
 // when the browser fetches a url
